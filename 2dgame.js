9b89837b5cef9748d574513a4a2fd877eb74d266
diamondstorm-leaderofchickenresistance <143503714+diamondstorm-leaderofchickenresistance@users.noreply.github.com>
2023-10-09 17:28:24 +0800

    Update 2dgame.js

--- 2dgame.js
+++ 2dgame.js
@@ -4,15 +4,20 @@
     ]
     var randomBackground = Math.floor(Math.random() + images.length);
     var selectedImage = images[randomBackground];
-    document.body.style.backgroundImage = "url('" + selectedImage + "')";
+    document.canvas.style.backgroundImage = "url('" + selectedImage + "')";
  };
+
+function fullscreen() {
+document.canvas.style.width = 100%;
+document.canvas.style.height = 97%;
+}
 const canvas = document.getElementById("myCanvas");
 const context = canvas.getContext("2d");
-canvas.onload = mainmenu()
+canvas.onload = mainmenu();
 function mainmenu {
 context.beginPath();
 context.rect(20, 40, 50, 50);
-context.fillStyle = "rgb(255, 128, 0)"
+context.fillStyle = "rgb(255, 128, 0)";
 context.fill();
 context.closePath();
 }
